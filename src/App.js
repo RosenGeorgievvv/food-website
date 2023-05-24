49eea9648a6bba5332b411f0cffdc8a6466877bf
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-05-24 16:49:44 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import './App.css';
 import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 function App() {
   return (
     <div className="App">
