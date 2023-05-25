b3435d2d896231eafa3ee985cd7cbea32a6705bb
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-05-25 17:35:40 +0300

    structuring the home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Home() {
   return (
-    <h1>This is the home page</h1>
+    <div className='home'>
+      <div className='container'>
+        <h1>Food & Taste Pizzeria</h1>
+        <p>PIZZA O FIT ANY TASTE</p>
+        <Link to='/menu'>
+          <button> ORDER NOW </button>
+        </Link>
+      </div>
+    </div>
   )
 }
 
