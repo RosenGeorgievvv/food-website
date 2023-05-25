50c1946d34c1c6392b5e6e9bf03db45f02497d6e
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-05-25 17:31:23 +0300

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,22 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../styles/Navbar.css'
 import logo from '../assets/logo.png';
 import { Link } from 'react-router-dom';
 import ReorderIcon from '@mui/icons-material/Reorder';
 
 function Navbar() {
+
+    const [visible, setVisible] = useState(false);
+
+    const toggleNavbar = () => {
+        setVisible(!visible);
+    }
+
     return (
         <div className='navbar'>
-            <div className='leftSide'>
+            <div className='leftSide' id={visible ? 'open' : 'close'}>
                 <img src={logo} />
+                <div className='hiddenLinks'>
+                    <Link to='/' >Home</Link>
+                    <Link to='/menu' >Menu</Link>
+                    <Link to='/about' >About</Link>
+                    <Link to='/contact' >Contact</Link>
+                </div>
             </div>
             <div className='rightSide'>
                 <Link to='/' >Home</Link>
                 <Link to='/menu' >Menu</Link>
                 <Link to='/about' >About</Link>
                 <Link to='/contact' >Contact</Link>
-                <button>
-                <ReorderIcon />
+                <button onClick={toggleNavbar}>
+                    <ReorderIcon />
                 </button>
             </div>
         </div>
