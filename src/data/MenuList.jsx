998c6da92e6101b1dc3a97ec7d9e821e786ade3b
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-05-26 14:49:32 +0300

    static data created

---
+++ src/data/MenuList.jsx
@@ -0,0 +1,27 @@
+import Pepperoni from '../assets/pepperoni.jpg';
+import Pineapple from '../assets/pineapple.jpg';
+import Margherita from '../assets/margherita.jpg';
+import Vegan from '../assets/vegan.jpg';
+
+export const MenuList = [
+    {
+        name: "Pepperoni Pizza",
+        image: Pepperoni,
+        price: 16.99
+    },
+    {
+        name: "Pineapple Pizza",
+        image: Pineapple,
+        price: 19.99
+    },
+    {
+        name: "Margherita Pizza",
+        image: Margherita,
+        price: 15.50
+    },
+    {
+        name: "Vegan Pizza",
+        image: Vegan,
+        price: 12.50
+    }
+]
