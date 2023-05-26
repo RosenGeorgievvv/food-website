7be4e0d7db7de2403e4676ed91a7863abadb7668
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-05-26 10:43:36 +0300

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from 'react';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import FacebookIcon from '@mui/icons-material/Facebook';
 
 function Footer() {
   return (
     <div className='footer'>
-        <div className='socialMedia'></div>
+        <div className='socialMedia'>
+            <InstagramIcon />
+            <FacebookIcon />
+        </div>
         <p>&copy; 2023 food&taste.com</p>
     </div>
   )
