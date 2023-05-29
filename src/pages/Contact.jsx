8c9eba3f5c8bb559b328aefa82b433f159326573
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-05-29 16:15:41 +0300

    Update Contact.jsx

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -10,9 +10,9 @@ function Contact() {
                 <h1>Contact Us</h1>
                 <form id='contact-form' method='POST'>
                     <label htmlFor='name'>Full Name</label>
-                    <input name='name' placeholder='Enter full name...' type='text' />
+                    <input name='name' placeholder='Enter full name...' type='text' required/>
                     <label htmlFor='email'>Email</label>
-                    <input name='email' placeholder='Enter email...' type='email' />
+                    <input name='email' placeholder='Enter email...' type='email' required/>
                     <label htmlFor='message'>Message</label>
                     <textarea rows="6" placeholder='Enter message...' name='message' required></textarea>
                     <button type='submit'>Send Message</button>
