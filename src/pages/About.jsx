38001c81682641686ca9d6686640f1d0f8af090e
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-05-29 15:36:05 +0300

    Update About.jsx

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
+import MultiplePizzas from '../assets/multiplePizzas.jpeg'
 
 function About() {
-  return (
-    <div className='about'>
-        
-    </div>
-  )
+    return (
+        <div className='about'>
+            <div className='aboutTop' style={{ backgroundImage: `url(${MultiplePizzas})` }}></div>
+            <div className='aboutBottom'>
+                <h1>About Us</h1>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                    Animi, fuga id rerum facilis ut cum harum consectetur cupiditate ex impedit?
+                    Sint corrupti, nulla molestiae odio illo neque cupiditate in sequi!</p>
+            </div>
+        </div>
+    )
 }
 
 export default About
